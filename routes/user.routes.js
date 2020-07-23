7b33aabe1930138b160a1a250014a4356ecb591d
metildachee <metildachee@gmail.com>
2020-07-23 22:17:51 +0800

    fixed some small bug

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,13 +5,18 @@ const checkUser = require('../setup/checkUser');
 // @desc displays the current user's published breakies
 router.get('/list', checkUser, (req, res) => {
     res.locals.atHomePage = false;
-    Users.find({ id: req.user._id, delete: false }).
+    Users.findById(req.user._id).
     populate({
         path: "publishes",
         populate: { path: "ingredients", model: "Ingredient" }
     }).
-    then( breakies => {
-        res.render("user/list", { breakies: breakies.publishes });
+    then( user => {
+        let availBreakies = [];
+        user.publishes.forEach( published => {
+            if (!published.deleted) 
+                availBreakies.push(published);
+        })
+        res.render("user/list", { breakies: availBreakies });
     }).
     catch(err => console.log(err));
 })
